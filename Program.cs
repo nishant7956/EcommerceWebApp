4256f44955f3025cbb8af820a75292c95ddb625c
Nishant Gupta <Nishant.9488@gmail.com>
2026-06-29 16:47:50 -0400

    Add Products REST API controller and register API controllers in Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
